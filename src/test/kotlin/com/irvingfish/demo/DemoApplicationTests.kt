75337685d720dee10787087df8abb480882e7de6
ujjeeq <irvingfish@gmail.com>
2018-09-05 08:38:12 +0200

    add integration test

--- src/test/kotlin/com/irvingfish/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/irvingfish/demo/DemoApplicationTests.kt
@@ -2,15 +2,30 @@ package com.irvingfish.demo
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@AutoConfigureWebTestClient
 class DemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    private val client = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build()
+
+    @Test
+    fun contextLoads() {
+    }
+
+    @Test
+    fun `Request HTTP API endpoint`() {
+        client.get().uri("/stars").exchange()
+                .expectStatus().is2xxSuccessful
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+    }
 
 }
